6ceb035f3727ff574015e030b98da10393d249b3
SirKuggorna <gandalhvi@gmail.com>
2016-03-02 13:54:29 +0200

    Edited Pressure.cpp and Pressure.h
    May need some work to make them compatible with LCPXpresso

--- Pressure.cpp
+++ Pressure.cpp
@@ -1,5 +1,5 @@
 /*
- * Fan.cpp
+ * Pressure.cpp
  *
  *  Created on: 1.3.2016
  *      Author: Haggis
@@ -17,16 +17,14 @@ Pressure::Pressure(int a){
  * returns pressure value
  * param: if true, doesn't read port, just return value
  */
-float Pressure::getValue(bool b ){
+float Pressure::getValue(bool b = 0){
 	if (!b) {
-			float aa = port->read();
-			aa = (aa / 4095) * 5;
-			aa = (1500 * aa) / (5 - aa);
-
-			value=(aa-1603)/11;
-			//printf("aa=%d, tt=%d\n", (int) aa, (int) value);
+			float pressure = port->read();
+			float scale = (pressure / /*SCALE FACTOR*/);
+			float edp = pressure * 0.95;
+			
+			
 
 		}
-	return 1;
 
 }
